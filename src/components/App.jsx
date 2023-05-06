35ff9f265dab8b1e6d9eac6fbfbe9a4330e22717
Ruslan <Giovingo92@gmail.com>
2023-05-06 15:09:06 +0300

    start my home work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import { Container, Section } from 'components';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Section></Section>
+    </Container>
   );
 };
